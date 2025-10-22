85e032c2d6effac894dcbeb4b05c342e866097ae
Semaja Voye <semaja.voye5_8@gmx.de>
2025-10-22 23:01:30 +0200

    Enhance comments for clarity and memory management in main.c

--- src/main.c
+++ src/main.c
@@ -11,10 +11,12 @@ struct Gate
     GateType type;
 
     // Inputs
+    // "Store the adress where the wire lives in memory"
     struct Wire *input1;
     struct Wire *input2;
 
     // Outputs
+    // "Store the adress where the wire lives in memory"
     struct Wire *output;
 };
 
@@ -31,6 +33,7 @@ int main() {
     // Allocating memory for our components 
 
     // Wires:
+    // *w_a is a pointer. malloc finds a block of memory big enough for one Wire struct and returns the adress of that block. w_a stores this addres (*)
     struct Wire *w_a = (struct Wire *)malloc(sizeof(struct Wire));
     struct Wire *w_b = (struct Wire *)malloc(sizeof(struct Wire));
     struct Wire *w_out = (struct Wire *)malloc(sizeof(struct Wire));
@@ -48,11 +51,14 @@ int main() {
     // Set our gate type
     g_nand->type = INVERT;
 
-    // "Connect" the wires
+    // "Connect" the wires (by copying the memory adresses)
+    // input1 now holds the adress of w_a wich is the wire struct.
+    // gate has no need to search the Wire because it now knows the Wire's adress (stored in w_a)
     g_nand->input1 = w_a;
     g_nand->input2 = w_b;
     g_nand->output = w_out;
 
+    // Dereference to the Wire struct and set its state to LOW
     w_a->state = LOW;
 
     print_status("Input A", w_a);
@@ -60,7 +66,11 @@ int main() {
     update_gate(g_nand);
     print_status("Output (Input A)", w_out);
 
-
+    // Add free() function for releasing the dynamically allocated memory of the wires and gates
+    free(w_a);
+    free(w_b);
+    free(w_out);
+    free(g_nand);
 
     return 0;
 }
