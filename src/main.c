5221d1053742476dc162f0df7a9d3a07d02a855b
Semaja Voye <semaja.voye5_8@gmx.de>
2025-10-23 21:38:26 +0200

    Refactor code structure for improved readability and maintainability
    Added Fonts to Display the Title of the simualtion

--- src/main.c
+++ src/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "logic.h"
 
+// Includes for SDL3
 #define SDL_MAIN_USE_CALLBACKS 1 // use the callbacks instead of main()
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+// Include logic gate simulation
+#include "logic.h"
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static TTF_Font *font = NULL;
+
+#define WINDOW_WIDTH 900
+#define WINDOW_HEIGHT 600
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -21,11 +29,26 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("VirtualLogiGate", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("VirtualLogiGate", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    if (!TTF_Init()) {
+        SDL_Log("Couldn't initialize SDL_ttf: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    #ifdef FONT_PATH
+    font = TTF_OpenFont(FONT_PATH, 24);
+    #endif
+
+    if (!font) {
+        SDL_Log("Couldn't load font: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
@@ -42,18 +65,42 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
-    /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
-    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
+    const int charsize = SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE;
 
-    /* clear the window to the draw color. */
-    SDL_RenderClear(renderer);
+    /* as you can see from this, rendering draws over whatever was drawn before it. */
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
+    SDL_RenderClear(renderer);  /* start with a blank canvas. */
 
-    /* put the newly-cleared rendering on the screen. */
-    SDL_RenderPresent(renderer);
+    // Create the title
+    char title_buffer[27];
+    size_t title_length = sizeof(title_buffer) - 1;
+    SDL_snprintf(title_buffer, sizeof(title_buffer), "Virtual LogiGate Simulator",
+                (unsigned long long)(SDL_GetTicks() / 1000));
+
+    SDL_Color text_color = { 255, 255, 255, 255 }; /* white full alpha */
+    SDL_Surface *text_surface = TTF_RenderText_Blended(font, title_buffer, title_length, text_color); /* anti-aliased text */
+
+    if (text_surface) {
+        SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+
+        if (text_texture) {
+            SDL_FRect dest_rect;
+            dest_rect.w = (float) text_surface->w;
+            dest_rect.h = (float) text_surface->h;
+            dest_rect.x = (WINDOW_WIDTH - text_surface->w) / 2.0f;
+            dest_rect.y = 400;
+
+            SDL_RenderTexture(renderer, text_texture, NULL, &dest_rect);
+
+            SDL_DestroyTexture(text_texture);
+        }
+
+        SDL_DestroySurface(text_surface);
+    }
+
+    SDL_RenderDebugTextFormat(renderer, (float) ((WINDOW_WIDTH - (charsize * 46)) / 2), 400, "(This program has been running for %" SDL_PRIu64 " seconds.)", SDL_GetTicks() / 1000);
+
+    SDL_RenderPresent(renderer);  /* put it all on the screen! */
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
@@ -61,5 +108,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    /* SDL will clean up the window/renderer for us. */
+    if (font) {
+        TTF_CloseFont(font);
+    }
+    TTF_Quit();
 }
